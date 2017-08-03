833e3899642c30f27d4541b4cab7740331e3a8fa
George Rosenberger <gr2578@cumc.columbia.edu>
2017-08-03 16:10:21 -0400

    [FIX] TRANSITION_ID Fix

--- src/Scores.cpp
+++ src/Scores.cpp
@@ -303,7 +303,7 @@ void Scores::reportOSW(const string& dataFN, const string& oswLevel) {
     if (oswLevel == "T") {
       insert_sql << "INSERT INTO " << table;
       insert_sql << " (FEATURE_ID, TRANSITION_ID, SCORE, QVALUE, PEP) VALUES (";
-      insert_sql <<  scoreIt->pPSM->getFeatureId() << ",";
+      insert_sql <<  scoreIt->pPSM->getId() << ","; // Note: Id already contains merged FEATURE_ID and TRANSITION_ID.
       insert_sql <<  scoreIt->score << ",";
       insert_sql <<  scoreIt->q << ",";
       insert_sql <<  scoreIt->pep << "); ";
